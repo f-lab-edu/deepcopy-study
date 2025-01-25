9c36535190f3c31e4f28364698fccdd44d3f6fd4
sunnykim91 <tjsgnl2002@gmail.com>
2025-01-25 21:23:40 +0900

    feat: 여러 입력에 대한 테스트 케이스 추가

--- src/test/deepCopy.test.ts
+++ src/test/deepCopy.test.ts
@@ -1,7 +1,7 @@
 import { deepCopy } from "../deepCopy";
 
-describe("deepCopy", () => {
-  test("깊은복사 테스트 1", () => {
+describe("깊은 복사 테스트 - 입력 A : 일반 객체", () => {
+  test("일반객체 테스트 1", () => {
     expect(deepCopy({ a: 1, b: 2, c: 3 })).toEqual({ a: 1, b: 2, c: 3 });
   });
   test("깊은복사 테스트 2", () => {
@@ -16,10 +16,33 @@ describe("deepCopy", () => {
       b: { c: 2, d: { e: 3 } },
     });
   });
-  test("깊은복사 테스트 4", () => {
-    expect(deepCopy({ a: 1, b: { c: 2, d: { e: { f: 3 } } } })).toEqual({
-      a: 1,
-      b: { c: 2, d: { e: { f: 3 } } },
-    });
+});
+
+describe("깊은 복사 테스트 - 입력 B : 배열", () => {
+  test("배열 테스트 1", () => {
+    expect(deepCopy([1, 2, 3])).toEqual([1, 2, 3]);
+  });
+  test("배열 테스트 2", () => {
+    expect(deepCopy([1, [2, 3]])).toEqual([1, [2, 3]]);
+  });
+  test("배열 테스트 3", () => {
+    expect(deepCopy([1, [2, [3]]])).toEqual([1, [2, [3]]]);
+  });
+});
+
+describe("깊은 복사 테스트 - 입력 C : null, undefined", () => {
+  test("null 테스트", () => {
+    expect(deepCopy(null)).toEqual(null);
+  });
+  test("undefined 테스트", () => {
+    expect(deepCopy(undefined)).toEqual(undefined);
+  });
+});
+
+describe("깊은 복사 테스트 - 입력 D : 순환 참조 객체", () => {
+  test("순환 참조 객체 테스트", () => {
+    const obj: any = { a: 1 };
+    obj.b = obj;
+    expect(deepCopy(obj)).toEqual({ a: 1, b: obj });
   });
 });
