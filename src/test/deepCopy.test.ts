693caf78412140214f810a216c7617237fd1ca76
sunnykim91 <tjsgnl2002@gmail.com>
2025-02-01 21:05:52 +0900

    refactor: 테스트 코드 그룹핑

--- src/test/deepCopy.test.ts
+++ src/test/deepCopy.test.ts
@@ -5,117 +5,131 @@ type CircularObject = {
   b?: CircularObject;
 };
 
-describe("복사가 잘 이루어 졌는가", () => {
-  test("일반 객체에 대한 테스트", () => {
-    const original = { a: 1, b: 2, c: 3 };
-    const copied = deepCopy(original);
-    expect(copied).toEqual(original);
+describe("deepCopy 함수 테스트", () => {
+  describe("복사가 제대로 이루어졌는가", () => {
+    describe("객체 테스트", () => {
+      test("일반 객체", () => {
+        const original = { a: 1, b: 2, c: 3 };
+        const copied = deepCopy(original);
+        expect(copied).toEqual(original);
+      });
+
+      test("중첩 객체", () => {
+        const original = { a: 1, b: { c: 2, d: 3 } };
+        const copied = deepCopy(original);
+        expect(copied).toEqual(original);
+      });
+
+      test("순환 참조 객체", () => {
+        const obj: CircularObject = { a: 1 };
+        obj.b = obj;
+        const copied = deepCopy(obj) as CircularObject;
+        expect(copied).toEqual(obj);
+      });
+    });
+
+    describe("배열 테스트", () => {
+      test("일차원 배열", () => {
+        const original = [1, 2, 3];
+        const copied = deepCopy(original);
+        expect(copied).toEqual(original);
+      });
+
+      test("이차원 배열", () => {
+        const original: number[][] = [1, [2, 3]] as number[][];
+        const copied = deepCopy(original) as number[][];
+        expect(copied).toEqual(original);
+      });
+    });
+
+    describe("특수 타입 테스트", () => {
+      test("null", () => {
+        expect(deepCopy(null)).toEqual(null);
+      });
+
+      test("undefined", () => {
+        expect(deepCopy(undefined)).toEqual(undefined);
+      });
+
+      test("Date", () => {
+        const date = new Date();
+        const copied = deepCopy(date);
+        expect(copied).toEqual(date);
+      });
+
+      test("정규표현식", () => {
+        const reg = /test/g;
+        const copied = deepCopy(reg);
+        expect(copied).toEqual(reg);
+      });
+    });
   });
 
-  test("중첩 객체에 대한 테스트", () => {
-    const original = { a: 1, b: { c: 2, d: 3 } };
-    const copied = deepCopy(original);
-    expect(copied).toEqual(original);
-  });
+  describe("복사본을 수정하였을때 원본객체에 영향을 끼치는가", () => {
+    describe("객체 테스트", () => {
+      test("일반 객체", () => {
+        const original = { a: 1, b: 2, c: 3 };
+        const copied = deepCopy(original);
+        copied.a = 4;
+        expect(original).not.toEqual(copied);
+      });
 
-  test("배열에 대한 테스트", () => {
-    const original = [1, 2, 3];
-    const copied = deepCopy(original);
-    expect(copied).toEqual(original);
-  });
+      test("중첩 객체", () => {
+        const original = { a: 1, b: { c: 2, d: 3 } };
+        const copied = deepCopy(original);
+        copied.b.c = 4;
+        expect(original).not.toEqual(copied);
+      });
 
-  test("이차원 배열에 대한 테스트", () => {
-    const original: number[][] = [1, [2, 3]] as number[][];
-    const copied = deepCopy(original) as number[][];
-    expect(copied).toEqual(original);
-  });
+      test("순환 참조 객체", () => {
+        const obj: CircularObject = { a: 1 };
+        obj.b = obj;
+        const copied = deepCopy(obj) as CircularObject;
+        copied.a = 2;
+        expect(obj).not.toEqual(copied);
+      });
+    });
 
-  test("null에 대한 테스트", () => {
-    expect(deepCopy(null)).toEqual(null);
-  });
+    describe("배열 테스트", () => {
+      test("일차원 배열", () => {
+        const original = [1, 2, 3];
+        const copied = deepCopy(original);
+        copied[0] = 4;
+        expect(original).not.toEqual(copied);
+      });
 
-  test("undefined에 대한 테스트", () => {
-    expect(deepCopy(undefined)).toEqual(undefined);
-  });
+      test("이차원 배열", () => {
+        const original: number[][] = [1, [2, 3]] as number[][];
+        const copied = deepCopy(original) as number[][];
+        copied[1][0] = 4;
+        expect(original).not.toEqual(copied);
+      });
+    });
 
-  test("순환 참조 객체에 대한 테스트", () => {
-    const obj: CircularObject = { a: 1 };
-    obj.b = obj;
-    const copied = deepCopy(obj) as CircularObject;
-    expect(copied).toEqual(obj);
-  });
+    describe("특수 타입 테스트", () => {
+      test("Date", () => {
+        const date = new Date();
+        const copied = deepCopy(date);
+        copied.setDate(4);
+        expect(date).not.toEqual(copied);
+      });
 
-  test("Date에 대한 테스트 ", () => {
-    const date = new Date();
-    const copied = deepCopy(date);
-    expect(copied).toEqual(date);
-  });
+      test("정규표현식", () => {
+        const reg = /test/g;
+        const copied = deepCopy(reg);
+        copied.lastIndex = 4;
+        expect(reg).not.toEqual(copied);
+      });
 
-  test("정규표현식에 대한 테스트", () => {
-    const reg = /test/g;
-    const copied = deepCopy(reg);
-    expect(copied).toEqual(reg);
-  });
-});
+      test("null", () => {
+        const copied = deepCopy(null);
+        expect(copied).toEqual(null);
+      });
 
-describe("복사한 객체의 속성 값을 바꿔도 원본 객체에 영향을 미치지 않는가", () => {
-  test("일반 객체에 대한 테스트", () => {
-    const original = { a: 1, b: 2, c: 3 };
-    const copied = deepCopy(original);
-    copied.a = 4;
-    expect(original).not.toEqual(copied);
-  });
-
-  test("중첩 객체에 대한 테스트", () => {
-    const original = { a: 1, b: { c: 2, d: 3 } };
-    const copied = deepCopy(original);
-    copied.b.c = 4;
-    expect(original).not.toEqual(copied);
-  });
-
-  test("배열에 대한 테스트", () => {
-    const original = [1, 2, 3];
-    const copied = deepCopy(original);
-    copied[0] = 4;
-    expect(original).not.toEqual(copied);
-  });
-
-  test("이차원 배열에 대한 테스트", () => {
-    const original: number[][] = [1, [2, 3]] as number[][];
-    const copied = deepCopy(original) as number[][];
-    copied[1][0] = 4;
-    expect(original).not.toEqual(copied);
-  });
-
-  test("순환 참조 객체에 대한 테스트", () => {
-    const obj: CircularObject = { a: 1 };
-    obj.b = obj;
-    const copied = deepCopy(obj) as CircularObject;
-    copied.a = 2;
-    expect(obj).not.toEqual(copied);
-  });
-
-  test("Date에 대한 테스트 ", () => {
-    const date = new Date();
-    const copied = deepCopy(date);
-    copied.setDate(4);
-    expect(date).not.toEqual(copied);
-  });
-
-  test("정규표현식에 대한 테스트", () => {
-    const reg = /test/g;
-    const copied = deepCopy(reg);
-    copied.lastIndex = 4;
-    expect(reg).not.toEqual(copied);
-  });
-
-  test("null에 대한 테스트", () => {
-    const copied = deepCopy(null);
-    expect(copied).toEqual(null);
-  });
-
-  test("undefined에 대한 테스트", () => {
-    const copied = deepCopy(undefined);
-    expect(copied).toEqual(undefined);
+      test("undefined", () => {
+        const copied = deepCopy(undefined);
+        expect(copied).toEqual(undefined);
+      });
+    });
   });
 });
